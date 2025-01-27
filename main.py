12268e164d730ba4c45f64c213408969db4dbec3
Bobr <kostyvorobev24@gmail.com>
2025-01-27 21:54:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+import requests
+import datetime
+import json
+import configparser
+from tqdm import tqdm
+
+congig = configparser.ConfigParser()
+congig.read('settings.ini')
+vk_tk = congig['TOKENS']['vk_token']
+yd_tk = congig['TOKENS']['yd_token']
+alb_id = congig['OTHER']['album_id']
+use_id = congig['OTHER']['user_id']
+
+
+class VK:
+    def __init__(self, token_vk, version='5.199'):
+        self.params = {
+            'access_token': token_vk,
+            'v': version
+        }
+        self.base = 'https://api.vk.com/method/'
+        self.all_list = []
+        self.json_list = []
+
+    def get_photos(self, user_id, maxcount=5):
+        url = f'{self.base}photos.get'
+        params = {
+            'owner_id': user_id,
+            'count': maxcount,
+            'album_id': alb_id,
+            'extended': 1
+        }
+        params.update(self.params)
+        response = requests.get(url, params=params)
+        kill = response.json()['response']['items']
+        for i in kill:
+            likes_num = i['likes']['count']             # взяли количество лайков у фотографии
+            if likes_num in self.all_list:
+                self.all_list.append(f'{likes_num}_{datetime.date.today()}')   # если кол-во лайков есть в списка добавляем дату
+            else:
+                self.all_list.append(likes_num)
+            list_size = []
+            for t in i['sizes']:
+                list_size.append(t['type'])
+            max_list = max(list_size)                    # буква МАХ фотографии
+            self.all_list.append(max_list)
+            if t['type'] == max_list:
+                list_url = (t['url'])                   # URL МАХ фотографии
+                self.all_list.append(list_url)
+
+    def create_folder(self, token, folder_name):
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        params = {
+            'path': folder_name
+            }
+        headers = {
+            'Authorization': token
+        }
+        response = requests.put(url, params=params, headers=headers)
+
+    def upload_file(self, token2, folder_name2):
+        i = 2
+        for nam in tqdm(self.all_list[::3]):
+            json_dict = {}
+            name_url = self.all_list[i]
+            filename = f'{nam}'+'.jpg'
+            json_dict = {"file_name": filename,
+                         "size": self.all_list[i-1]}
+            self.json_list.append(json_dict)
+            headers = {'Authorization': token2}
+            params = {'path': f'/{folder_name2}/' + filename, 'url': name_url}
+            response = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload', headers=headers,
+                                     params=params)
+            i += 3
+
+        with open('result.json', 'w', encoding='utf-8') as f:
+            json.dump(self.json_list, f, ensure_ascii=False, indent=0, separators=(',', ': '))
+
+
+vk_connector = VK(vk_tk)
+
+vk_connector.get_photos(use_id)
+vk_connector.create_folder(yd_tk, use_id)
+vk_connector.upload_file(yd_tk, use_id)
